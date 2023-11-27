cfc1a4864bcf13d247437a16ee7f13dce4fddefc
SantiPMendoza <14santi3c@gmail.com>
2023-11-27 17:47:51 +0100

    OnlyZoo

---
+++ OnlyZoo/OnlyZoo/CatalogoMascotas.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OnlyZoo
+{
+    public partial class CatalogoMascotas : Form
+    {
+        public CatalogoMascotas()
+        {
+            InitializeComponent();
+        }
+
+        private void CatalogoMascotas_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
