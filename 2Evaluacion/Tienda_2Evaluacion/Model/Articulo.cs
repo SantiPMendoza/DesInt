f412564a3cdeadfeec1d5c25fbad9b0ff26cb146
Vespertino <Vespertino@C09PC20.informatica.comercio>
2024-01-17 21:42:27 +0100

    Tienda chetada

---
+++ 2Evaluacion/Tienda_2Evaluacion/Model/Articulo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tienda_2Evaluacion.Model
+{
+    public class Articulo
+    {
+        public int Id { get; set; }
+        public SportType Deporte { get; set; }
+        public ArticleType TipoArticulo { get; set; }
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
+
+        public enum SportType
+        {
+            Futbol,
+            Baloncesto,
+            Tenis,
+            Esqui,
+            Boxeo
+        }
+
+        public enum ArticleType
+        {
+            Accesorio,
+            Ropa,
+            Zapatillas,
+            Articulo
+        }
+    }
+}
---
+++ 2Evaluacion/Tienda_2Evaluacion/Utils/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tienda_2Evaluacion.Utils
+{
+    internal class Constants
+    {
+        public const string CONNECTION_STRING = "server=localhost;port=1818;uid=root;pwd=root;database=tienda_deportes;";
+    }
+}
